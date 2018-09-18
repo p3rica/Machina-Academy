d1fe7e3a72e5f8935aa1c032c912937a1dbc2687
Petar <sta.petargmail.com>
2018-09-18 22:24:32 +0200

    prvi dodatak u projekat

--- 2.predavanje/2.predavanje/Source.cpp
+++ 2.predavanje/2.predavanje/Source.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
+int integer;
+float floatingpointnumber;
+bool boolean;
+char character;
+
 int main()
 {
+	sizeof(integer);
+	sizeof(floatingpointnumber);
+	sizeof(boolean);
+	sizeof(character);
 
 }
