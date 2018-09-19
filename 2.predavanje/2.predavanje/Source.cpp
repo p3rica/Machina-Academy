d1dabdf180c73c46950e216874bf8a29b08ed3ab
Petar <sta.petargmail.com>
2018-09-19 22:08:54 +0200

    dodao switch

--- 2.predavanje/2.predavanje/Source.cpp
+++ 2.predavanje/2.predavanje/Source.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
+using namespace std;
 
+//Varijable(primitivni tipovi)
 int integer;
 float floatingpointnumber;
 bool boolean;
 char character;
+
+
+/*int velicina(int x, int y)
+{
+	
+	int result = x + y;
+
+	return result;
+}*/
+
+int rezultat(int x)
+{
+	switch (x)
+	{
+	case 1:
+		cout << "pobjeda!!!!" << endl;
+		return x;
+
+	case 2:
+		cout << "gubitak" << endl;
+		return x;
+
+	default:
+		cout << "neispravan unos" << endl;
+		return x;
+	}
+}
+
+
 
 int main()
 {
@@ -12,4 +43,8 @@ int main()
 	sizeof(boolean);
 	sizeof(character);
 
+	//int kraj = velicina(2, 7);
+	int kraj = rezultat(4);
+	cout << kraj << endl;
+	system("pause");
 }
