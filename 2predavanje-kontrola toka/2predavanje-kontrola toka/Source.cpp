9217e497b45411491fe2d9c91d0bde080a0d2d91
Petar <sta.petargmail.com>
2018-09-20 13:26:42 +0200

    if zavrsio poceo petlje

---
+++ 2predavanje-kontrola toka/2predavanje-kontrola toka/Source.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+	
+	
+ //Kontrola toka
+	
+
+	/*int result = 2;
+	if (result == 1)
+	{
+		cout << "pobjeda" << endl;
+
+	}
+
+	else
+
+	if (result == 2)
+	{
+		cout << "gubitak" << endl;
+	}
+	
+	else
+	{
+		cout << "netocan unos" << endl;
+	}*/
+
+
+	// Petlje:
+	//for
+	//while
+	//do while
+
+	//FOR
+	cout << "***********************************************************FOR********************************************" << endl;
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << i << endl;
+	}
+	
+	//WHILE
+	cout << "***********************************************************WHILE********************************************" << endl;
+	int y = 0;
+	while (y < 10)
+	{
+		cout << y << endl;
+		y++;
+	}
+
+	cin.get();
+}
